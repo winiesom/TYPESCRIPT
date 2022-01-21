700aab9421a1497e46287be80c522eb8ac0e4ac7
mmaesom <winifred.esomonu@stetis.com>
2022-01-21 15:35:15 +0100

    generics, enums, tuples

--- public/app.js
+++ public/app.js
@@ -12,19 +12,58 @@ const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === "invoice") {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "end");
 });
-// Generics
+// GENERICS
 const addUID = (obj) => {
     let uid = Math.floor(Math.random() * 100);
     return Object.assign(Object.assign({}, obj), { uid });
 };
 let docOne = addUID({ name: "yoshi", age: 40 });
-console.log(docOne);
+console.log(docOne.age);
+const docThree = {
+    uid: 1,
+    resourceName: "person",
+    data: { name: "shaun" },
+};
+const docFour = {
+    uid: 2,
+    resourceName: "shoppingList",
+    data: ["bread", "milk", "toilet roll"],
+};
+console.log(docThree, docFour);
+// ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docFive = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: "name of the wind" },
+};
+const docSix = {
+    uid: 10,
+    resourceType: ResourceType.PERSON,
+    data: { name: "yoshi" },
+};
+console.log(docFive, docSix);
+// tuples
+let tup = ["ryu", 25, true];
+tup[0] = "ken";
+tup[1] = 30;
+let student;
+student = ["chun-li", 223423];
--- src/app.ts
+++ src/app.ts
@@ -18,21 +18,85 @@ const list = new ListTemplate(ul);
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number];
+  values = [toFrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
   if (type.value === "invoice") {
-    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
 
   list.render(doc, type.value, "end");
 });
 
-// Generics
-const addUID = (obj: object) => {
+// GENERICS
+const addUID = <T extends { name: string }>(obj: T) => {
   let uid = Math.floor(Math.random() * 100);
   return { ...obj, uid };
 };
 
 let docOne = addUID({ name: "yoshi", age: 40 });
-console.log(docOne);
+
+console.log(docOne.age);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: "person",
+  data: { name: "shaun" },
+};
+
+const docFour: Resource<string[]> = {
+  uid: 2,
+  resourceName: "shoppingList",
+  data: ["bread", "milk", "toilet roll"],
+};
+
+console.log(docThree, docFour);
+
+// ENUMS
+
+enum ResourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR,
+  PERSON,
+}
+
+interface ResourceTwo<T> {
+  uid: number;
+  resourceType: ResourceType;
+  data: T;
+}
+
+const docFive: ResourceTwo<object> = {
+  uid: 1,
+  resourceType: ResourceType.BOOK,
+  data: { title: "name of the wind" },
+};
+
+const docSix: ResourceTwo<object> = {
+  uid: 10,
+  resourceType: ResourceType.PERSON,
+  data: { name: "yoshi" },
+};
+
+console.log(docFive, docSix);
+
+// tuples
+
+let tup: [string, number, boolean] = ["ryu", 25, true];
+tup[0] = "ken";
+tup[1] = 30;
+
+let student: [string, number];
+student = ["chun-li", 223423];
